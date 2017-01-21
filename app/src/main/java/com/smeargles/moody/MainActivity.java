089935458f99c278c144def846c110e9598331d1
Steven Au <steventinwing@gmail.com>
2017-01-21 08:51:15 -0800

    Added working face recognition api

--- app/src/main/java/com/smeargles/moody/MainActivity.java
+++ app/src/main/java/com/smeargles/moody/MainActivity.java
@@ -1,15 +1,20 @@
 package com.smeargles.moody;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    public final static String EXTRA_MESSAGE = "gg.mood20.app.MESSAGE";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,5 +53,17 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    /** Called when the user clicks the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+    public void activityRecognize(View v) {
+        Intent intent = new Intent(this, RecognizeActivity.class);
+        startActivity(intent);
     }
 }
