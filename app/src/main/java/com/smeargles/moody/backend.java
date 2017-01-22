3f682cde1a19fa41525bdd0ca78ddc12e79e8ec8
Steven Au <steventinwing@gmail.com>
2017-01-22 05:34:14 -0800

    added graph view

--- app/src/main/java/com/smeargles/moody/backend.java
+++ app/src/main/java/com/smeargles/moody/backend.java
@@ -1,7 +1,12 @@
 package com.smeargles.moody;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
+
 
 public class backend extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class backend extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_backend);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
+
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
     }
+
 }
